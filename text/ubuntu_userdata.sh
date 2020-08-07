6b55cade96268b6d360d8e1d350486a61b38dc80
Luca Pinna <>
2020-08-07 16:08:10 +0200

    remove extra text from user data

--- text/ubuntu_userdata.sh
+++ text/ubuntu_userdata.sh
@@ -7,9 +7,6 @@ exec 1> >(logger -s -t $(basename $0)) 2>&1
 export LC_ALL=C.UTF-8
 export DEBIAN_FRONTEND=noninteractive
 
-# Ensure SSM installed on Amazon Linux
-# in cases where it is not available / removed
-
 apt-get update
 apt-get -y install python-setuptools python-pip
 pip install awscli --upgrade
